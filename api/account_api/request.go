06d3b87de39d5c1471e3fae464e0653751481e22
tiEFLand <109794591+tiEFLand@users.noreply.github.com>
2020-10-23 08:18:57 +0000

    - Bumped the version number

--- api/account_api/request.go
+++ api/account_api/request.go
@@ -1,4 +1,6 @@
 package account_api
 
 type GetAccountRequest struct {
-	Ex         string `bson:"ex" 
+	Ex         string `bson:"ex" json:"ex" binding:"required"`
+	TickerType string `bson:"ticker_type" json:"ticker_type" binding:"required"`
+	Ticker     string `b
