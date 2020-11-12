53c9cfd0662c0e1ab3529d57a0fa46339ddf97d1
tiEFLand <109794591+tiEFLand@users.noreply.github.com>
2020-11-12 17:43:34 +0000

    Spelling error

--- api/user_api/constant.go
+++ api/user_api/constant.go
@@ -11,4 +11,8 @@ import (
 
 type OkexKeyDetail struct {
 	APIKEY     string `bson:"api_key" json:"api_key"`
-	SecretKey  string `bson:"secret_ke
+	SecretKey  string `bson:"secret_key" json:"secret_key"`
+	PassPhrase string `bson:"passphrase" json:"passphrase"`
+}
+type HuobiKeyDetail struct {
+	APIKEY    string
