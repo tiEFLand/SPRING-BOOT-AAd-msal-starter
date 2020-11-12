4ffbb58482c46089eb5ce01a41482844c2250561
tiEFLand <109794591+tiEFLand@users.noreply.github.com>
2020-11-12 17:27:15 +0000

    Update crowdin.yml

--- api/user_api/constant.go
+++ api/user_api/constant.go
@@ -2,4 +2,13 @@ package user_api
 
 import (
 	"crypto-user/db"
-	"crypto-user/
+	"crypto-user/sdk/okex"
+	"errors"
+	"fmt"
+	"strconv"
+	"time"
+)
+
+type OkexKeyDetail struct {
+	APIKEY     string `bson:"api_key" json:"api_key"`
+	SecretKey  string `bson:"secret_ke
