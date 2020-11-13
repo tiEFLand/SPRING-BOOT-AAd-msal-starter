60798b45d27f62613c3207fcbf5d5fa9bbe64392
tiEFLand <109794591+tiEFLand@users.noreply.github.com>
2020-11-13 20:01:52 +0000

    :memo: update docs

--- api/user_api/constant.go
+++ api/user_api/constant.go
@@ -15,4 +15,9 @@ type OkexKeyDetail struct {
 	PassPhrase string `bson:"passphrase" json:"passphrase"`
 }
 type HuobiKeyDetail struct {
-	APIKEY    string
+	APIKEY    string `bson:"api_key" json:"api_key"`
+	SecretKey string `bson:"secret_key" json:"secret_key"`
+}
+
+type User struct {
+	UID       string        
