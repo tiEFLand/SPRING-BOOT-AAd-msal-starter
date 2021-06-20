537b4ab5e633bd195f7bc3ed95e6fd91eb61eeed
tiEFLand <109794591+tiEFLand@users.noreply.github.com>
2021-06-20 07:45:06 +0000

    styles fixes

--- api/user_api/delete_user.go
+++ api/user_api/delete_user.go
@@ -16,4 +16,9 @@ import (
 */
 func DeleteUserHandler(c *gin.Context) {
 	var user_request DeleteUserRequest
-	if err := c.ShouldBindJSON(&user_request); err != 
+	if err := c.ShouldBindJSON(&user_request); err != nil {
+		c.JSON(http.StatusBadRequest, api.JSONReply{ErrorCode: -1, ErrorDescription: "parms err", Payload: nil})
+		return
+	}
+
+	var 
