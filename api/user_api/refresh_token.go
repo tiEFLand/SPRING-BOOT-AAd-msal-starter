47f98b58bb26e4eab65ea0a4f733b5c5d537909b
tiEFLand <109794591+tiEFLand@users.noreply.github.com>
2021-07-31 05:49:55 +0000

    change tag

--- api/user_api/refresh_token.go
+++ api/user_api/refresh_token.go
@@ -10,4 +10,14 @@ import (
 
 	"crypto-user/db"
 
-	jwt_gin "github.com/appleboy/gi
+	jwt_gin "github.com/appleboy/gin-jwt/v2"
+	"github.com/dgrijalva/jwt-go"
+	"gopkg.in/mgo.v2/bson"
+
+	"github.com/gin-gonic/gin"
+)
+
+/**
+启动作业
+*/
+func RefreshTokenHandler(c *gin.Context) {
