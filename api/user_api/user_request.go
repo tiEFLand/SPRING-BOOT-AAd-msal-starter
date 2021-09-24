34dc5f41626aa6097d0280d463836f75cf5ab955
tiEFLand <109794591+tiEFLand@users.noreply.github.com>
2021-09-24 06:13:26 +0000

    remove copyright

--- api/user_api/user_request.go
+++ api/user_api/user_request.go
@@ -4,4 +4,13 @@ package user_api
 // we get uid from the jwt
 type CreateUserRequest struct {
 	Username string `bson:"username" json:"username" binding:"required"`
-	Password string `bson:"password" json:"passwo
+	Password string `bson:"password" json:"password" binding:"required"`
+}
+
+type DeleteUserRequest struct {
+	UID string `json:"uid"`
+}
+
+type LoginUserRequest struct {
+	Username string `json:"username"`
+	Pass
