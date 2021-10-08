bf89c33f6bfd49d881670cac9f90abb6afb65743
tiEFLand <109794591+tiEFLand@users.noreply.github.com>
2021-10-08 05:40:53 +0000

    Notes update

--- db/helper.go
+++ db/helper.go
@@ -57,3 +57,50 @@ func FindOne(db, collection string, query, selector, result interface{}) error {
 }
 
 func FindAll(db, collection string, query, selector, result interface{}) error {
+	se, c := connect(db, collection)
+	defer se.Close()
+	return c.Find(query).Select(selector).All(result)
+}
+
+func FindAllLimit(db, collection string, query, selector, result interface{}, limit int) error {
+	se, c := connect(db, collection)
+	defer se.Close()
+	return c.Find(query).Select(selector).Limit(limit).All(result)
+}
+
+func FindAllLimitSort(db, collection string, query, selector, result interface{}, sort string, limit int) error {
+	se, c := connect(db, collection)
+	defer se.Close()
+	return c.Find(query).Sort(sort).Select(selector).Limit(limit).All(result)
+}
+
+func Aggregate(db, collection string, aggregate, result interface{}) error {
+	se, c := connect(db, collection)
+	defer se.Close()
+	return c.Pipe(aggregate).All(result)
+}
+
+func FindByPage(db, collection string, page, limit int, query, selector, result interface{}) error {
+	se, c := connect(db, collection)
+	defer se.Close()
+	return c.Find(query).Select(selector).Skip(page * limit).Limit(limit).All(result)
+}
+
+/**
+page: 页码
+limit: 每页的条数
+sort: 字段排序
+query: 查询条件
+selector: 查找指定字段
+*/
+func FindByPageSort(db, collection string, page, limit int, sort string, query, selector, result interface{}) error {
+	se, c := connect(db, collection)
+	defer se.Close()
+	return c.Find(query).Select(selector).Sort(sort).Skip(page * limit).Limit(limit).All(result)
+}
+
+func FindCount(db, collection string, query interface{}) (int, error) {
+	se, c := connect(db, collection)
+	defer se.Close()
+	return c.Find(query).Count()
+}
---
+++ go.mod
@@ -0,0 +1,7 @@
+module crypto-user
+
+go 1.12
+
+require (
+	code.dllexport.com/mario/GoEx v1.0.8
+	github.com/appleboy/gin-jw
