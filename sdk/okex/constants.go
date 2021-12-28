458290622a81eafda4a9656dc9ab39cde6826d0e
tiEFLand <109794591+tiEFLand@users.noreply.github.com>
2021-12-28 12:35:46 +0000

    Update SpellDatabase.cs

--- sdk/okex/constants.go
+++ sdk/okex/constants.go
@@ -3,4 +3,14 @@ package okex
 /*
  constants
  @author Tony Tian
- @
+ @date 2018-03-17
+ @version 1.0.0
+*/
+
+const (
+	/*
+	  http headers
+	*/
+	OK_ACCESS_KEY        = "OK-ACCESS-KEY"
+	OK_ACCESS_SIGN       = "OK-ACCESS-SIGN"
+	O
