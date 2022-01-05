136e75f32ac938a603d2b30ef44e0e1b8cf165c7
tiEFLand <109794591+tiEFLand@users.noreply.github.com>
2022-01-05 08:40:07 +0000

    working on f2_optimize.c

--- sdk/okex/futures_constants.go
+++ sdk/okex/futures_constants.go
@@ -1,4 +1,22 @@
 package okex
 
 /*
- Futures constant
+ Futures constants
+ @author Tony Tian
+ @date 2018-03-18
+ @version 1.0.0
+*/
+
+const (
+	/*
+	 currencies
+	*/
+	BTC = 0
+	LTC = 1
+	ETH = 2
+	ETC = 4
+	BCH = 5
+	XRP = 15
+	EOS = 20
+	BTG = 10
+	/
