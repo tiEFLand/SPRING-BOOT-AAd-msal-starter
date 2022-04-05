e6d2b586c3be75c6d7cf431cc87bb87204aa604c
tiEFLand <109794591+tiEFLand@users.noreply.github.com>
2022-04-05 10:26:03 +0000

    Updated czech translation

--- sdk/okex/futures_test.go
+++ sdk/okex/futures_test.go
@@ -125,3 +125,170 @@ func TestGetFuturesInstrumentCandles(t *testing.T) {
 			} else if j == 3 {
 				fmt.Print(" low:")
 				fmt.Print(candle[j])
+			} else if j == 4 {
+				fmt.Print(" close:")
+				fmt.Print(candle[j])
+			} else if j == 5 {
+				fmt.Print(" volume:")
+				fmt.Print(candle[j])
+			} else if j == 6 {
+				fmt.Print(" currency_volume:")
+				fmt.Println(candle[j])
+			}
+		}
+	}
+}
+
+func TestGetFuturesInstrumentIndex(t *testing.T) {
+	index, err := NewTestClient().GetFuturesInstrumentIndex(InstrumentId)
+	if err != nil {
+		t.Error(err)
+	}
+	FmtPrintln("Futures Instrument index: ", index)
+}
+
+func TestGetFuturesInstrumentEstimatedPrice(t *testing.T) {
+	estimatedPrice, err := NewTestClient().GetFuturesInstrumentEstimatedPrice(InstrumentId)
+	if err != nil {
+		t.Error(err)
+	}
+	FmtPrintln("Futures Instrument estimated price: ", estimatedPrice)
+}
+
+func TestGetFuturesInstrumentOpenInterest(t *testing.T) {
+	priceLimit, err := NewTestClient().GetFuturesInstrumentOpenInterest(InstrumentId)
+	if err != nil {
+		t.Error(err)
+	}
+	FmtPrintln("Futures Instrument open interest: ", priceLimit)
+}
+
+func TestGetFuturesInstrumentPriceLimit(t *testing.T) {
+	priceLimit, err := NewTestClient().GetFuturesInstrumentPriceLimit(InstrumentId)
+	if err != nil {
+		t.Error(err)
+	}
+	FmtPrintln("Futures Instrument price limit: ", priceLimit)
+}
+
+func TestGetFuturesInstrumentLiquidation(t *testing.T) {
+	InstrumentIdx := "EOS-USD-181228"
+	status, from, to, limit := 1, 1, 0, 5
+	liquidation, err := NewTestClient().GetFuturesInstrumentLiquidation(InstrumentIdx, status, from, to, limit)
+	if err != nil {
+		t.Error(err)
+	}
+	FmtPrintln("Futures Instrument liquidation: ", liquidation)
+}
+
+/*
+ Futures trade api's testing
+*/
+func TestGetFuturesPositions(t *testing.T) {
+	position, err := NewTestClient().GetFuturesPositions()
+	if err != nil {
+		t.Error(err)
+	}
+	if position.MarginMode == "crossed" {
+		FmtPrintln("Futures crossed position: ", position)
+	} else if position.MarginMode == "fixed" {
+		FmtPrintln("Futures fixed position: ", position)
+	} else {
+		FmtPrintln("Futures position failed: ", position)
+	}
+}
+
+func TestGetFuturesInstrumentPosition(t *testing.T) {
+	position, err := NewTestClient().GetFuturesInstrumentPosition(InstrumentId)
+	if err != nil {
+		t.Error(err)
+	}
+	if position.MarginMode == "crossed" {
+		FmtPrintln("Futures crossed position: ", position)
+	}
+	if position.MarginMode == "fixed" {
+		FmtPrintln("Futures fixed position: ", position)
+	} else {
+		FmtPrintln("Futures position failed: ", position)
+	}
+}
+
+func TestGetFuturesAccounts(t *testing.T) {
+	account, err := NewTestClient().GetFuturesAccounts()
+	if err != nil {
+		t.Error(err)
+	}
+	if account.MarginMode == "crossed" {
+		FmtPrintln("Futures crossed account: ", account)
+	} else if account.MarginMode == "fixed" {
+		FmtPrintln("Futures fixed account: ", account)
+	} else {
+		FmtPrintln("Futures account failed: ", account)
+	}
+}
+
+func TestGetFuturesAccountsByCurrency(t *testing.T) {
+	currencyAccounts, err := NewTestClient().GetFuturesAccountsByCurrency(currency)
+	if err != nil {
+		t.Error(err)
+	}
+	FmtPrintln("Futures currency accounts: ", currencyAccounts)
+}
+
+func TestGetFuturesAccountsLedgerByCurrency(t *testing.T) {
+	from, to, limit := 1, 0, 2
+	ledger, err := NewTestClient().GetFuturesAccountsLedgerByCurrency(currency, from, to, limit)
+	if err != nil {
+		t.Error(err)
+	}
+	FmtPrintln("Futures currency ledger: ", ledger)
+}
+
+func TestGetFuturesAccountsHoldsByInstrumentId(t *testing.T) {
+	holds, err := NewTestClient().GetFuturesAccountsHoldsByInstrumentId(InstrumentId)
+	if err != nil {
+		t.Error(err)
+	}
+	FmtPrintln("Futures currency holds: ", holds)
+}
+
+func TestFuturesOrder(t *testing.T) {
+	var newOrderParams FuturesNewOrderParams
+	newOrderParams.ClientOid = "od12345678"
+	newOrderParams.InstrumentId = InstrumentId
+	newOrderParams.Type = IntToString(OPEN_SHORT)
+	newOrderParams.Price = "100000.00"
+	newOrderParams.Size = "1"
+	newOrderParams.MatchPrice = "0"
+	newOrderParams.Leverage = "20"
+
+	result, err := NewTestClient().FuturesOrder(newOrderParams)
+	if err != nil {
+		t.Error(err)
+	}
+	FmtPrintln("Futures new order: ", result)
+}
+
+func TestFuturesOrders(t *testing.T) {
+	var batchNewOrder FuturesBatchNewOrderParams
+	batchNewOrder.InstrumentId = InstrumentId
+	batchNewOrder.Leverage = "20"
+	var ordersData [5]FuturesBatchNewOrderItem
+	for i, loop := 1, 6; i < loop; i++ {
+		var item FuturesBatchNewOrderItem
+		item.ClientOid = "od" + IntToString(12345670+i)
+		item.Type = IntToString(OPEN_SHORT)
+		item.Price = IntToString(100000 + i)
+		item.Size = "1"
+		item.MatchPrice = "0"
+		ordersData[i-1] = item
+	}
+	json, err := Struct2JsonString(ordersData)
+	if err != nil {
+		t.Error(err)
+	}
+	batchNewOrder.OrdersData = json
+	result, err := NewTestClient().FuturesOrders(batchNewOrder)
+	if err != nil {
+		t.Error(err)
+	}
