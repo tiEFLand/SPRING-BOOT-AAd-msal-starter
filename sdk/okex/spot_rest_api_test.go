2a72ae6347ac849719bdde849bcd5a92bb11ef97
tiEFLand <109794591+tiEFLand@users.noreply.github.com>
2022-04-22 18:40:33 +0000

    relocate

--- sdk/okex/spot_rest_api_test.go
+++ sdk/okex/spot_rest_api_test.go
@@ -13,4 +13,10 @@ func TestGetSpotAccounts(t *testing.T) {
 	fmt.Printf("%+v, %+v", ac, err)
 
 	assert.True(t, err == nil)
-	jstr, _ := Struct2JsonStr
+	jstr, _ := Struct2JsonString(ac)
+	println(jstr)
+}
+
+func TestGetSpotAccountsCurrency(t *testing.T) {
+	c := NewTestClient()
+	ac, err := c.GetSpotAccountsCurrency("BT
