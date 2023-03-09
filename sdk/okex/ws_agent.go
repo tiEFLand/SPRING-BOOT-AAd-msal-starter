3f665e058fe540e9c47bb9c3d4031998a7a1a4bf
tiEFLand <109794591+tiEFLand@users.noreply.github.com>
2023-03-09 18:51:32 +0000

    Bump websocket-extensions from 0.1.3 to 0.1.4

--- sdk/okex/ws_agent.go
+++ sdk/okex/ws_agent.go
@@ -85,3 +85,155 @@ func (a *OKWSAgent) Start(config *Config) error {
 			go a.finalize()
 		}
 		return nil
+	}
+}
+
+func (a *OKWSAgent) Subscribe(channel, filter string, cb ReceivedDataCallback) error {
+	a.processMut.Lock()
+	defer a.processMut.Unlock()
+
+	st := SubscriptionTopic{channel, filter}
+	bo, err := subscribeOp([]*SubscriptionTopic{&st})
+	if err != nil {
+		return err
+	}
+
+	msg, err := Struct2JsonString(bo)
+	// log.Printf("Send Msg: %s", msg)
+	if err := a.conn.WriteMessage(websocket.TextMessage, []byte(msg)); err != nil {
+		return err
+	}
+
+	cbs := a.subMap[st.channel]
+	if cbs == nil {
+		cbs = []ReceivedDataCallback{}
+		a.activeChannels[st.channel] = false
+	}
+	cbs = append(cbs, cb)
+	a.subMap[st.channel] = cbs
+	fullTopic, err := st.ToString()
+	a.subMap[fullTopic] = cbs
+
+	a.cache = WSSubscriptionCache{Channel: channel, Filter: filter, Callback: cb}
+	return nil
+}
+
+func (a *OKWSAgent) UnSubscribe(channel, filter string) error {
+	a.processMut.Lock()
+	defer a.processMut.Unlock()
+
+	st := SubscriptionTopic{channel, filter}
+	bo, err := unsubscribeOp([]*SubscriptionTopic{&st})
+	if err != nil {
+		return err
+	}
+
+	msg, err := Struct2JsonString(bo)
+	log.Printf("Send Msg: %s", msg)
+	if err := a.conn.WriteMessage(websocket.TextMessage, []byte(msg)); err != nil {
+		return err
+	}
+
+	a.subMap[channel] = nil
+	a.activeChannels[channel] = false
+
+	return nil
+}
+
+func (a *OKWSAgent) Login(apiKey, passphrase string) error {
+
+	timestamp := EpochTime()
+
+	preHash := PreHashString(timestamp, GET, "/users/self/verify", "")
+	if sign, err := HmacSha256Base64Signer(preHash, a.config.SecretKey); err != nil {
+		return err
+	} else {
+		op, err := loginOp(apiKey, passphrase, timestamp, sign)
+		data, err := Struct2JsonString(op)
+		log.Printf("Send Msg: %s", data)
+		err = a.conn.WriteMessage(websocket.TextMessage, []byte(data))
+		if err != nil {
+			return err
+		}
+		time.Sleep(time.Millisecond * 100)
+	}
+	return nil
+}
+
+func (a *OKWSAgent) keepalive() {
+	a.ping()
+}
+
+func (a *OKWSAgent) Stop() error {
+	defer func() {
+		a := recover()
+		log.Printf("Stop End. Recover msg: %+v", a)
+	}()
+
+	close(a.stopCh)
+	return nil
+}
+
+func (a *OKWSAgent) finalize() error {
+	defer func() {
+		log.Printf("Finalize End. Connection to WebSocket is closed.")
+		log.Printf("sending cache for reconnect: ", a.cache)
+		ReconnectCh <- &a.cache
+	}()
+
+	select {
+	case <-a.stopCh:
+		if a.conn != nil {
+			close(a.errCh)
+			close(a.wsTbCh)
+			close(a.wsEvtCh)
+			close(a.wsErrCh)
+			return a.conn.Close()
+		}
+	}
+
+	return nil
+}
+
+func (a *OKWSAgent) ping() {
+	msg := "ping"
+	//log.Printf("Send Msg: %s", msg)
+	a.conn.WriteMessage(websocket.TextMessage, []byte(msg))
+}
+
+func (a *OKWSAgent) GzipDecode(in []byte) ([]byte, error) {
+	reader := flate.NewReader(bytes.NewReader(in))
+	defer reader.Close()
+
+	return ioutil.ReadAll(reader)
+}
+
+func (a *OKWSAgent) handleErrResponse(r interface{}) error {
+	log.Printf("handleErrResponse %+v \n", r)
+	return nil
+}
+
+func (a *OKWSAgent) handleEventResponse(r interface{}) error {
+	er := r.(*WSEventResponse)
+	a.activeChannels[er.Channel] = (er.Event == CHNL_EVENT_SUBSCRIBE)
+	return nil
+}
+
+func (a *OKWSAgent) handleTableResponse(r interface{}) error {
+	tb := ""
+	switch r.(type) {
+	case *WSTableResponse:
+		tb = r.(*WSTableResponse).Table
+	case *WSDepthTableResponse:
+		tb = r.(*WSDepthTableResponse).Table
+	}
+
+	cbs := a.subMap[tb]
+	if cbs != nil {
+		for i := 0; i < len(cbs); i++ {
+			cb := cbs[i]
+			if err := cb(r); err != nil {
+				return err
+			}
+		}
+	}
