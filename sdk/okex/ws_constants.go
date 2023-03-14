3c45971a995215fef4e319c33289f206f028f95f
tiEFLand <109794591+tiEFLand@users.noreply.github.com>
2023-03-14 13:44:14 +0000

    Include

--- sdk/okex/ws_constants.go
+++ sdk/okex/ws_constants.go
@@ -7,4 +7,10 @@ package okex
  @version 1.0.0
 */
 
-import "er
+import "errors"
+
+const (
+	WS_API_HOST = "okexcomreal.bafang.com:10442"
+	WS_API_URL  = "wss://real.okex.com:10442/ws/v3"
+
+	CHNL_FUTURES_TICKER          = "futures/tic
