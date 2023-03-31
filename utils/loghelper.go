4acd0068415e5fe94a838872622d17fd7ce9044a
tiEFLand <109794591+tiEFLand@users.noreply.github.com>
2023-03-31 15:53:31 +0000

    update package files

--- utils/loghelper.go
+++ utils/loghelper.go
@@ -8,4 +8,11 @@ import (
 func GetLogger() *Logger {
 	level, _ := GetConfig().Get("log.level")
 	if level == "" {
-		level
+		level = "info"
+	}
+	logger := NewLogger()
+	debugHandle, err := NewRotatingFileHandler("server.log")
+	if err != nil {
+		log.Fatalf("打开日志文件失败：%+v", err)
+	}
+	if string
