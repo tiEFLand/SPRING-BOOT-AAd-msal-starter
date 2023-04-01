62c9e7513cb116199697cf4090073e591c7423e8
tiEFLand <109794591+tiEFLand@users.noreply.github.com>
2023-04-01 17:30:07 +0000

    build project

--- utils/redis.go
+++ utils/redis.go
@@ -12,4 +12,8 @@ type RedisUtils struct {
 }
 
 func (r *RedisUtils) Connect() {
-	url, _ := G
+	url, _ := GetConfig().Get("redis.url")
+	c, err := redis.DialURL(url)
+	for err != nil {
+		logger.Error(err)
+		time.Sleep(t
